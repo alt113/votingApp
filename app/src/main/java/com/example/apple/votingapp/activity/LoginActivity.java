7183a8353339b0cba085766be3816fa6528b2016
Jihad Khurfan <j.khurfan@tecfrac.com>
2019-11-27 19:47:17 +0200

    moved activities to activity package

--- app/src/main/java/com/example/apple/votingapp/activity/LoginActivity.java
+++ app/src/main/java/com/example/apple/votingapp/activity/LoginActivity.java
@@ -1,4 +1,4 @@
-package com.example.apple.votingapp;
+package com.example.apple.votingapp.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,6 +11,7 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.example.apple.votingapp.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
--- app/src/main/java/com/example/apple/votingapp/activity/ResetPasswordActivity.java
+++ app/src/main/java/com/example/apple/votingapp/activity/ResetPasswordActivity.java
@@ -1,4 +1,4 @@
-package com.example.apple.votingapp;
+package com.example.apple.votingapp.activity;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -10,6 +10,7 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.example.apple.votingapp.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
