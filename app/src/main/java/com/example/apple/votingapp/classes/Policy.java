240ee3245c197ad63330fc74a8ac10c62afb8b69
Jihad Khurfan <j.khurfan@tecfrac.com>
2019-12-09 10:58:14 +0200

    changed class naming

--- app/src/main/java/com/example/apple/votingapp/classes/Policy.java
+++ app/src/main/java/com/example/apple/votingapp/classes/Policy.java
@@ -6,15 +6,25 @@ public class Policy implements Serializable {
 
     private String title;
     private String description;
-    private Long vote;
+    private Long count1;
+    private Long count2;
+    private Long count3;
+    private Long option1;
+    private Long option2;
+    private Long option3;
 
     public Policy() {
     }
 
-    public Policy(String title, String description, Long vote) {
+    public Policy(String title, String description, Long count1, Long count2, Long count3, Long option1, Long option2, Long option3) {
         this.title = title;
         this.description = description;
-        this.vote = vote;
+        this.count1 = count1;
+        this.count2 = count2;
+        this.count3 = count3;
+        this.option1 = option1;
+        this.option2 = option2;
+        this.option3 = option3;
     }
 
     public String getTitle() {
@@ -33,11 +43,51 @@ public class Policy implements Serializable {
         this.description = description;
     }
 
-    public Long getVote() {
-        return vote;
+    public Long getCount1() {
+        return count1;
     }
 
-    public void setVote(Long vote) {
-        this.vote = vote;
+    public void setCount1(Long count1) {
+        this.count1 = count1;
+    }
+
+    public Long getCount2() {
+        return count2;
+    }
+
+    public void setCount2(Long count2) {
+        this.count2 = count2;
+    }
+
+    public Long getCount3() {
+        return count3;
+    }
+
+    public void setCount3(Long count3) {
+        this.count3 = count3;
+    }
+
+    public Long getOption1() {
+        return option1;
+    }
+
+    public void setOption1(Long option1) {
+        this.option1 = option1;
+    }
+
+    public Long getOption2() {
+        return option2;
+    }
+
+    public void setOption2(Long option2) {
+        this.option2 = option2;
+    }
+
+    public Long getOption3() {
+        return option3;
+    }
+
+    public void setOption3(Long option3) {
+        this.option3 = option3;
     }
 }
